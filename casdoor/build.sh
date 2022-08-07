fa3495f7bb02cbef186015812b0d506023c2c154
WarlockFish <941510759@qq.com>
2022-08-08 01:57:05 +0800

    add casdoor

---
+++ casdoor/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#try to connect to google to determine whether user need to use proxy
+curl google.com -o /dev/null --connect-timeout 5 2> /dev/null
+if [ $? == 0 ]
+then
+    echo "Successfully connected to Google, no need to use Go proxy"
+    GOOS=linux go mod tidy
+    GOOS=linux go build -ldflags="-linkmode external -extldflags '-static' -w -s" -o server .
+else
+    echo "Google is blocked, Go proxy is enabled: GOPROXY=https://goproxy.cn,direct"
+    GOOS=linux GOPROXY=https://goproxy.cn,direct go mod tidy
+    #GOOS=linux GOPROXY=https://goproxy.cn,direct go build -ldflags="-linkmode external -extldflags '-static' -w -s" -o server .
+    CGO_ENABLED=1 GOOS=linux GOPROXY=https://goproxy.cn,direct go build -ldflags="-linkmode internal -extldflags '-static' -w -s" -o server .
+fi
